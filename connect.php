67fce42d83091b2f98ff173fba4c23e8022dc97d
kanokwatt <cmuthailand@hotmail.com>
2018-11-02 08:27:58 +0700

    Connect Database

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+// connect.php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "company";
+
+// 1. Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+//echo "Connect 555!";
+?>
